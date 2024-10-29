5d0c672e8b713baef28ef4dd4d660fbae09d530a
nelliejellie <emekaewelike@gmail.com>
2024-10-29 12:05:31 +0100

    second commit

--- product/main.py
+++ product/main.py
@@ -6,10 +6,26 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import mode
 from typing import List
+from passlib.context import CryptContext
 
-app = FastAPI()
+app = FastAPI(title="Products Api",description="get info for products",terms_of_service="",
+      contact={
+          "Developer Name":"Emeka Ewelike",
+          "Email":"Emeka Ewelike"
+      },
+      license_info={
+        "name":"blah",
+
+      },
+      docs_url="/docs"
+      )
 
 models.Base.metadata.create_all(engine)
+
+pwd_context = CryptContext(
+      schemes=["bcrypt"],
+      deprecated="auto",
+    )
 
 def get_db():
     db = SessionLocal()
@@ -19,36 +35,44 @@ def get_db():
         db.close()
 
 
-@app.post('/product',status_code=status.HTTP_201_CREATED)
+@app.post('/product',status_code=status.HTTP_201_CREATED,tags=["Products"])
 def add(request: schemas.Product, db: Session = Depends(get_db)):
-    new_product = models.Product(name=request.name, description=request.description, price=request.price)
+    new_product = models.Product(name=request.name, description=request.description, price=request.price,seller_id=1)
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
     return request
 
-@app.put('/product/{id}', response_model=schemas.DisplayProduct)
+@app.put('/product/{id}', response_model=schemas.DisplayProduct,tags=["Products"])
 def update(id,response:Response, request:schemas.Product,db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id) 
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='product not found')
-        response.status_code = status.HTTP_404_NOT_FOUND
     product.update(request.dict())
     db.commit()
     return {'product successfully updated'}
 
-@app.get('/products', response_model=List[schemas.DisplayProduct])
+@app.get('/products', response_model=List[schemas.DisplayProduct],tags=["Products"])
 def products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return products
 
-@app.get('/product/{id}')
+@app.get('/product/{id}',tags=["Products"])
 def product(id,db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
     return product
 
-@app.delete('/product/{id}')
+@app.delete('/product/{id}',tags=["Products"])
 def delete(id,db: Session = Depends(get_db)):
     db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
     db.commit()
     return {"product deleted"}
+
+@app.post('/seller', response_model=schemas.DisplaySeller,tags=["Sellers"])
+def create_seller(request:schemas.Seller,db: Session = Depends(get_db)):
+    hashpassword = pwd_context.hash(request.password)
+    new_seller = models.Seller(username=request.username, email=request.email,password=hashpassword)
+    db.add(new_seller)
+    db.commit()
+    db.refresh(new_seller)
+    return new_seller
--- product/schemas.py
+++ product/schemas.py
@@ -3,11 +3,25 @@ from pydantic import BaseModel
 class Product(BaseModel):
     name:str
     description:str
-    price:str
+    price:int
 
-class DisplayProduct(Product):
-    # if you want this to be the response
-    # description:str
-    # price:str
+class DisplaySeller(BaseModel):
+    username:str
+    email:str
+
     class Config:
         orm_mode = True
+
+class DisplayProduct(BaseModel):
+    # if you want this to be the response
+    description:str
+    price:int
+    seller: DisplaySeller
+    class Config:
+        orm_mode = True
+
+class Seller(BaseModel):
+    username:str
+    email: str
+    password:str
+
